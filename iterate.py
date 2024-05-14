032247ed518b5a118e44bebb2aaa9ca1ebe4b113
shruthigundla <shruthi.gundla@jukshio.com>
2024-05-14 12:48:55 +0530

    Update iterate.py

--- iterate.py
+++ iterate.py
@@ -1,3 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument ('--number', help = "enter the numbers")
+args = parser.parse_args()
 num = list(range(10))
 previousNum = 0
 for i in num:
