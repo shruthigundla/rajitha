12ca66862e378e46bf9bb24eb42f91ba484b7cf1
shruthigundla <shruthi.gundla@jukshio.com>
2024-05-14 12:50:08 +0530

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,10 +1,18 @@
+
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
- 
-img = cv.imread('/home/shruthi-gundla/Downloads/avngers.jpg')
-cv.imwrite("/home/shruthi-gundla/Desktop/shruthi/histogramgraph.png",img)
-assert img is not None, "file could not be read, check with os.path.exists()"
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--image_path", help = "Enter the path of your image")
+parser.add_argument("--out_dir", help = "name of output directory where you want to save your output")
+
+args = vars (parser.parse_args())
+image =cv.imread(args['image_path'])
+
+img = cv.imread('bird.jpg')
+assert img is not None, "file could not be read, check with os.path.exists()" 
 color = ('b','g','r')
 for i,col in enumerate(color):
  histr = cv.calcHist([img],[i],None,[256],[0,256])
